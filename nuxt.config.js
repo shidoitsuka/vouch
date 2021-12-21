b3cd46114891a554c58efa01bc09fb362bdf0991
Brian Harianja <hi@shdev.tech>
2021-12-22 00:44:25 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
     ],
      server: {
       cors: {
-        credentials: true, // "Configures the Access-Control-Allow-Credentials CORS header. Set to true to pass the header, otherwise it is omitted."
+        credentials: false, // "Configures the Access-Control-Allow-Credentials CORS header. Set to true to pass the header, otherwise it is omitted."
         origin: ['https://gossamer-spiffy-uncle.glitch.me/'] // Array of whitelisted origin(s)
       }  
      }
